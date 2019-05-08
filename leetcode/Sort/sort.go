65728b635f4415fb510c70105f749545900de22e
shaocong <shaocong_mo@163.com>
2019-05-08 19:03:50 +0800

    sort

--- leetcode/Sort/sort.go
+++ leetcode/Sort/sort.go
@@ -47,17 +47,22 @@ func InsertSort(nums []int) {
 }
 
 // 希尔排序
-func ShellSort(nums []int) {
-	gap := len(nums) / 2
-	for gap > 0 {
-		for i, j := 0, gap; i < len(nums)-gap && j < len(nums); i, j = i+1, j+1 {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
+func ShellSort(array []int) {
+	n := len(array)
+	if n < 2 {
+		return
+	}
+	key := n / 2
+	for key > 0 {
+		for i := key; i < n; i++ {
+			j := i
+			for j >= key && array[j] < array[j-key] {
+				array[j], array[j-key] = array[j-key], array[j]
+				j = j - key
 			}
 		}
-		gap = gap / 2
+		key = key / 2
 	}
-	InsertSort(nums)
 }
 
 // 归并排序
@@ -71,34 +76,25 @@ func MergeSort(nums []int) []int {
 }
 
 func merge(left []int, right []int) []int {
-	result := make([]int, len(left)+len(right))
-	index, leftIndex, rightIndex := 0, 0, 0
+	result := []int{}
+	leftIndex, rightIndex := 0, 0
 	for leftIndex < len(left) && rightIndex < len(right) {
 		if left[leftIndex] > right[rightIndex] {
-			result[index] = right[rightIndex]
+			result = append(result, right[rightIndex])
 			rightIndex++
 		} else {
-			result[index] = left[leftIndex]
+			result = append(result, left[leftIndex])
 			leftIndex++
 		}
-		index++
 	}
-	for leftIndex < len(left) {
-		result[index] = left[leftIndex]
-		index++
-		leftIndex++
-	}
-	for rightIndex < len(right) {
-		result[index] = right[rightIndex]
-		index++
-		rightIndex++
-	}
+	result = append(result, left[leftIndex:]...)
+	result = append(result, right[rightIndex:]...)
 	return result
 }
 
 // 快速排序
 func QuickSort(nums []int) {
-	quickSort(nums, 0, len(nums)-1)
+	qsort(nums)
 }
 
 func quickSort(nums []int, left int, right int) {
@@ -132,4 +128,176 @@ func partition(nums []int, left int, right int) int {
 		pivot = left
 	}
 	return pivot
+}
+
+// from https://zh.wikipedia.org/wiki/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F#Go
+func qsort(data []int) {
+	if len(data) <= 1 {
+		return
+	}
+	mid := data[0]
+	head, tail := 0, len(data)-1
+	for i := 1; i <= tail; {
+		if data[i] > mid {
+			data[i], data[tail] = data[tail], data[i]
+			tail--
+		} else {
+			data[i], data[head] = data[head], data[i]
+			head++
+			i++
+		}
+	}
+	qsort(data[:head])
+	qsort(data[head+1:])
+}
+
+// 堆排序
+// 用一维数组代表树
+// 父节点 i
+// 父节点的左子节点 2*i + 1
+// 父节点的右子节点 2*i + 2
+func HeapSort(nums []int) {
+	heapSize := len(nums)
+	heapNum := heapSize / 2
+	// 构建最大堆
+	for i := heapNum; i > -1; i-- {
+		maxHeap(nums, i, heapSize-1)
+	}
+	for i := heapSize - 1; i > -1; i-- {
+		// 把最大堆的堆顶（最大值）放到数组最后
+		nums[i], nums[0] = nums[0], nums[i]
+		// 重新构建最大堆 新堆大小 = 旧堆大小 - 1 （即不对已排好的数组尾巴进行堆处理）
+		maxHeap(nums, 0, i-1)
+	}
+}
+
+func maxHeap(nums []int, i int, end int) {
+	l := 2*i + 1
+	if l > end {
+		return
+	}
+	n := l
+	r := 2*i + 2
+	if r <= end && nums[r] > nums[l] {
+		n = r
+	}
+	if nums[i] > nums[n] {
+		return
+	}
+	nums[i], nums[n] = nums[n], nums[i]
+	maxHeap(nums, n, end)
+}
+
+// 计数排序
+// 找出待排序的数组中最大和最小的元素
+// 统计数组中每个值为 i的元素出现的次数，存入数组C 的第  i项
+// 对所有的计数累加（从 C 中的第一个元素开始，每一项和前一项相加）
+// 反向填充目标数组：将每个元素 i放在新数组的第 C[i] 项，每放一个元素就将 C[i] 减去1
+func CountingSort(nums []int) {
+	size := len(nums)
+	if size <= 0 {
+		return
+	}
+	min, max := nums[0], nums[0]
+	for i := 1; i < size; i++ {
+		if nums[i] > max {
+			max = nums[i]
+		} else if nums[i] < min {
+			min = nums[i]
+		}
+	}
+	countSize := max - min + 1
+	count := make([]int, countSize)
+	for i := 0; i < size; i++ {
+		count[nums[i]-min]++
+	}
+	for j, i := 0, 0; j < countSize; {
+		if count[j] != 0 {
+			count[j]--
+			nums[i] = j + min
+			i++
+		} else {
+			j++
+		}
+
+	}
+}
+
+// 桶排序
+func BucketSort(nums []int) {
+	size := len(nums)
+	if size <= 0 {
+		return
+	}
+	min, max := nums[0], nums[0]
+	for i := 1; i < size; i++ {
+		if nums[i] > max {
+			max = nums[i]
+		} else if nums[i] < min {
+			min = nums[i]
+		}
+	}
+	// 依据实际情况设定桶的容量
+	bucketSize := 5
+	bucketnum := (max-min)/bucketSize + 1
+	bucket := make([][]int, bucketnum)
+	for i := 0; i < bucketnum; i++ {
+		bucket[i] = []int{}
+	}
+	for i := 0; i < size; i++ {
+		index := (nums[i] - min) / bucketSize
+		bucket[index] = append(bucket[index], nums[i])
+	}
+	index := 0
+	for i := 0; i < bucketnum; i++ {
+		InsertSort(bucket[i])
+		for j := 0; j < len(bucket[i]); j++ {
+			nums[index] = bucket[i][j]
+			index++
+		}
+	}
+}
+
+// 基数排序
+func RedixSort(nums []int) {
+	// index [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+	// redix [_, -9, -8, -7, -6, -5, -4, -3, -2, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9]
+	size := len(nums)
+	if size <= 0 {
+		return
+	}
+	max := nums[0]
+	for i := 1; i < size; i++ {
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	exp := 1
+	for max/exp > 0 {
+		redix := make([][]int, 20)
+		for i := 0; i < 20; i++ {
+			redix[i] = []int{}
+		}
+		pushToRedix(nums, exp, redix)
+		exp = exp * 10
+	}
+}
+
+func pushToRedix(nums []int, remNum int, redix [][]int) {
+	index := -1
+	for i := 0; i < len(nums); i++ {
+		if nums[i]*nums[i] >= remNum%10-1 {
+			if index == -1 {
+				index = i
+			}
+			redixIndex := nums[i]/remNum%10 + 10
+			redix[redixIndex] = append(redix[redixIndex], nums[i])
+		}
+	}
+	for i := index; i < len(redix); i++ {
+		for j := 0; j < len(redix[i]); j++ {
+			nums[index] = redix[i][j]
+			index++
+		}
+	}
 }
--- leetcode/Sort/sort_test.go
+++ leetcode/Sort/sort_test.go
@@ -9,6 +9,8 @@ func TestBubbleSort(t *testing.T) {
 	testBubbleSortLogf([]int{3, 1}, t)
 	testBubbleSortLogf([]int{1, 3}, t)
 	testBubbleSortLogf([]int{}, t)
+	testBubbleSortLogf([]int{-1, 3}, t)
+	testBubbleSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testBubbleSortLogf(nums []int, t *testing.T) {
@@ -25,6 +27,8 @@ func TestSelectSort(t *testing.T) {
 	testSelectSortLogf([]int{3, 1}, t)
 	testSelectSortLogf([]int{1, 3}, t)
 	testSelectSortLogf([]int{}, t)
+	testSelectSortLogf([]int{-1, 3}, t)
+	testSelectSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testSelectSortLogf(nums []int, t *testing.T) {
@@ -41,6 +45,8 @@ func TestInsertSort(t *testing.T) {
 	testInsertSortLogf([]int{3, 1}, t)
 	testInsertSortLogf([]int{1, 3}, t)
 	testInsertSortLogf([]int{}, t)
+	testInsertSortLogf([]int{-1, 3}, t)
+	testInsertSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testInsertSortLogf(nums []int, t *testing.T) {
@@ -58,6 +64,8 @@ func TestShellSort(t *testing.T) {
 	testShellSortLogf([]int{3, 1}, t)
 	testShellSortLogf([]int{1, 3}, t)
 	testShellSortLogf([]int{}, t)
+	testShellSortLogf([]int{-1, 3}, t)
+	testShellSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testShellSortLogf(nums []int, t *testing.T) {
@@ -75,6 +83,8 @@ func TestMergeSort(t *testing.T) {
 	testMergeSortLogf([]int{3, 1}, t)
 	testMergeSortLogf([]int{1, 3}, t)
 	testMergeSortLogf([]int{}, t)
+	testMergeSortLogf([]int{-1, 3}, t)
+	testMergeSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testMergeSortLogf(nums []int, t *testing.T) {
@@ -91,11 +101,92 @@ func TestQuickSort(t *testing.T) {
 	testQuickSortLogf([]int{3, 1}, t)
 	testQuickSortLogf([]int{1, 3}, t)
 	testQuickSortLogf([]int{}, t)
+	testQuickSortLogf([]int{-1, 3}, t)
+	testQuickSortLogf([]int{3, -1, 1, 2, -9}, t)
 }
 
 func testQuickSortLogf(nums []int, t *testing.T) {
 	t.Logf("Input: %v", nums)
 	QuickSort(nums)
+	t.Logf("Output: %v", nums)
+	t.Logf("----------------")
+}
+
+func TestHeapSort(t *testing.T) {
+	testHeapSortLogf([]int{8, 9, 1, 7, 2, 3, 5, 4, 6, 0}, t)
+	testHeapSortLogf([]int{5, 4, 2, 3, 8}, t)
+	testHeapSortLogf([]int{1, 2, 3}, t)
+	testHeapSortLogf([]int{3, 2, 1}, t)
+	testHeapSortLogf([]int{3, 1}, t)
+	testHeapSortLogf([]int{1, 3}, t)
+	testHeapSortLogf([]int{}, t)
+	testHeapSortLogf([]int{-1, 3}, t)
+	testHeapSortLogf([]int{3, -1, 1, 2, -9}, t)
+}
+
+func testHeapSortLogf(nums []int, t *testing.T) {
+	t.Logf("Input: %v", nums)
+	HeapSort(nums)
+	t.Logf("Output: %v", nums)
+	t.Logf("----------------")
+}
+
+func TestCountingSort(t *testing.T) {
+	testCountingSortLogf([]int{8, 9, 1, 7, 2, 3, 5, 4, 6, 0}, t)
+	testCountingSortLogf([]int{5, 4, 2, 3, 8}, t)
+	testCountingSortLogf([]int{1, 2, 3}, t)
+	testCountingSortLogf([]int{3, 2, 1}, t)
+	testCountingSortLogf([]int{3, 1}, t)
+	testCountingSortLogf([]int{1, 3}, t)
+	testCountingSortLogf([]int{}, t)
+	testCountingSortLogf([]int{-1, 3}, t)
+	testCountingSortLogf([]int{3, -1, 1, 2, -9}, t)
+}
+
+func testCountingSortLogf(nums []int, t *testing.T) {
+	t.Logf("Input: %v", nums)
+	CountingSort(nums)
+	t.Logf("Output: %v", nums)
+	t.Logf("----------------")
+}
+
+func TestBucketSort(t *testing.T) {
+	testBucketSortLogf([]int{8, 9, 1, 7, 2, 3, 5, 4, 6, 0}, t)
+	testBucketSortLogf([]int{8, 9, 1, 7, 2, 33, 5, 4, 6, 100}, t)
+	testBucketSortLogf([]int{5, 4, 2, 3, 8}, t)
+	testBucketSortLogf([]int{1, 2, 3}, t)
+	testBucketSortLogf([]int{3, 2, 1}, t)
+	testBucketSortLogf([]int{3, 1}, t)
+	testBucketSortLogf([]int{1, 3}, t)
+	testBucketSortLogf([]int{}, t)
+	testBucketSortLogf([]int{-1, 3}, t)
+	testBucketSortLogf([]int{3, -1, 1, 2, -9}, t)
+}
+
+func testBucketSortLogf(nums []int, t *testing.T) {
+	t.Logf("Input: %v", nums)
+	BucketSort(nums)
+	t.Logf("Output: %v", nums)
+	t.Logf("----------------")
+}
+
+func TestRedixSort(t *testing.T) {
+	testRedixSortLogf([]int{8, 9, 1, 7, 2, 3, 5, 4, 6, 0}, t)
+	testRedixSortLogf([]int{8, 9, 1, 7, 2, 33, 5, 4, 6, 100}, t)
+	testRedixSortLogf([]int{5, 4, 2, 3, 8}, t)
+	testRedixSortLogf([]int{1, 2, 3}, t)
+	testRedixSortLogf([]int{3, 2, 1}, t)
+	testRedixSortLogf([]int{3, 1}, t)
+	testRedixSortLogf([]int{1, 3}, t)
+	testRedixSortLogf([]int{1, 0, 3}, t)
+	testRedixSortLogf([]int{}, t)
+	testRedixSortLogf([]int{-1, 3}, t)
+	testRedixSortLogf([]int{3, -1, 1, 2, -9}, t)
+}
+
+func testRedixSortLogf(nums []int, t *testing.T) {
+	t.Logf("Input: %v", nums)
+	RedixSort(nums)
 	t.Logf("Output: %v", nums)
 	t.Logf("----------------")
 }
